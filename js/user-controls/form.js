aebfd11b255355d796da2107f97a7c6099decb45
Victor Herrera <dsvictorh@gmail.com>
2014-07-30 11:18:38 -0600

    Change event

--- js/user-controls/form.js
+++ js/user-controls/form.js
@@ -98,9 +98,10 @@ define(['js/exception'], function(exception){
 			self.form = $('#' + args.form);
 
 			if(self.form && self.form.is('form')){
-				self.form.on('keypress', 'input', function(event){
-					if(event.keyCode == 13)
+				self.form.on('keyup', 'input', function(event){
+					if(event.keyCode == 13){
 			    		self.send();
+					}
 			    });
 			}else{
 				throw new exception.Exception('#' + args.form + ' is not a valid form element');
